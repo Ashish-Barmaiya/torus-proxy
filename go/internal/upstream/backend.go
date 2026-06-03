324b2f9bcef8441b33c8aa0cbafed9ab259ccb0b
Ashish-Barmaiya <ashishbarmaiya2908@gmail.com>
2026-06-03 10:12:24 +0530

    refactor(go): implement custom connection pool for reverse proxy

--- go/internal/upstream/backend.go
+++ go/internal/upstream/backend.go
@@ -1,9 +1,11 @@
 package upstream
 
 import (
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 )
 
 type Backend struct {
@@ -19,6 +21,23 @@ func NewBackend(targetUrl string) (*Backend, error) {
 	}
 
 	proxy := httputil.NewSingleHostReverseProxy(u)
+
+	// Custom connection pool
+	customTransport := &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:          10000,
+		MaxIdleConnsPerHost:   2000,
+		IdleConnTimeout:       90 * time.Second,
+		DisableKeepAlives:     false,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		ResponseHeaderTimeout: 5 * time.Second,
+	}
+
+	proxy.Transport = customTransport
 
 	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
 		http.Error(w, "Bad Gateway", http.StatusBadGateway)
